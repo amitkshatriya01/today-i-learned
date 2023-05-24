7eaa424e181d7e4b2659b1125614194db7d630e7
Amitsingh Kshatriya <kshatriya.amit.singh@gmail.com>
2023-05-23 22:05:35 -0500

    First react components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import "./style.css";
 function App() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
-        <h1>Today I Learned</h1>
-      </div>
+    <>
+      {/* Header */}
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            height="68"
+            width="68"
+            alt="Today I Learned Logo"
+          />
+          <h1>Today I Learned</h1>
+        </div>
 
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+      <NewFactForm />
+      <main class="main">
+        <CategoryFilters />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Face form</form>;
+}
+
+function CategoryFilters() {
+  return <aside>Category filter</aside>;
+}
+
+function FactList() {
+  return <section>Facts List</section>;
 }
 
 export default App;
